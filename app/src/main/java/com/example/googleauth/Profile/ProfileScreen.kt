8844ac1060e5037ef88fcf6e5b3d1c60ee8b5e9c
cdg <chitransh210703@gmail.com>
2024-04-24 15:15:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/googleauth/Profile/ProfileScreen.kt
@@ -0,0 +1,38 @@
+package com.example.googleauth.Profile
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.googleauth.presentation.UserData
+
+@Composable
+fun ProfileScreen(
+    userData: UserData?,
+    onSignOut: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if(userData?.username != null) {
+            Text(
+                text = userData.username,
+                textAlign = TextAlign.Center,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Button(onClick = onSignOut) {
+            Text(text = "Sign out")
+        }
+    }
+}
